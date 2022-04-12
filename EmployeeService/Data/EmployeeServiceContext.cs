db5a8a9f5345977a5e1318e18b247aa8ad3c33d7
mriyanhidayat <riyanmh17@gmail.com>
2022-04-13 05:13:47 +0700

    Learn API .NET with Swagger

---
+++ EmployeeService/Data/EmployeeServiceContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeService.Models;
+
+namespace EmployeeService.Data
+{
+    public class EmployeeServiceContext : DbContext
+    {
+        public EmployeeServiceContext (DbContextOptions<EmployeeServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeService.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeService/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeService.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int ContactNumber { get; set; }
+
+    }
+}
